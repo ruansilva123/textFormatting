94361779ab670706e000183d2615169537157810
ruansilva123 <ruancplay59@gmail.com>
2024-04-23 11:51:46 -0300

    feat: realizando modificacoes na toolbar e salvando mensagem no banco

--- home/views.py
+++ home/views.py
@@ -1,7 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import json
+from .models import Texto
 
 def home(request):
-    return render(request, 'home.html')
+    texto = Texto.objects.all()
+    return render(request, 'home.html', {'textos': texto})
 
 def salvar(request): 
-    pass 
+    data = json.loads(request.body)
+    texto = Texto(conteudo_texto=data['texto'])
+    texto.save()
+    return HttpResponse('1') #Retorna 1 se o processo deu certo
